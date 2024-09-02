ced9a55a5b36c7739d15f10f360bf076d760c593
figgou <90537366+figgou@users.noreply.github.com>
2024-09-01 22:01:08 -0300

    Add files via upload

---
+++ C39076Ex01.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj39076_L4_Ex01
+{
+    internal class C39076Ex01
+    {
+        static void Main(string[] args)
+        {
+            int valor1, valor2;
+
+            string repetir = "S";
+
+            while (repetir == "S")
+            {
+                try
+                {
+                    Console.Write("Insira o primeiro valor: ");
+                    valor1 = int.Parse(Console.ReadLine());
+
+                    Console.Write("Insira o segundo valor: ");
+                    valor2 = int.Parse(Console.ReadLine());
+                }
+
+                catch
+                {
+                    Console.Write("Insira um número!");
+                    Console.ReadKey();
+                    return;
+                }
+
+                while (valor1 >= valor2)
+                {
+                    Console.Write("Insira um valor maior que o anterior: ");
+                    valor2 = int.Parse(Console.ReadLine());
+
+                }
+
+                Console.Write("Deseja repetir? (S/N)");
+                repetir = Console.ReadLine();
+                Console.Clear();
+            }
+
+        }
+    }
+}
---
+++ C39076_L4_Ex03.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj39076_L4_Ex03
+{
+    internal class C39076_L4_Ex03
+    {
+        static void Main(string[] args)
+        {
+            string resposta = "S";
+
+            int n1, n2, res;
+
+            while (resposta == "S")
+            {
+                Console.Write("Insira o primeiro valor entre 1 e 100: ");
+                n1 = int.Parse(Console.ReadLine());
+
+                while (n1 < 0 || n1 > 100)
+                {
+                    Console.Write("Insira um valor válido: ");
+                    n1 = int.Parse(Console.ReadLine());
+                }
+
+                Console.Write("Insira o segundo valor entre 1 e 100: ");
+                n2 = int.Parse(Console.ReadLine());
+
+                while (n2 < 0 || n2 > 100)
+                {
+                    Console.Write("Insira um valor válido: ");
+                    n2 = int.Parse(Console.ReadLine());
+                }
+
+                res = n1 + n2;
+
+                Console.WriteLine("A soma dos números é: " + res);
+
+                Console.Write("Deseja repetir? (S/N): ");
+                resposta = Console.ReadLine();
+            }
+
+            
+
+
+
+
+
+        }
+    }
+}
