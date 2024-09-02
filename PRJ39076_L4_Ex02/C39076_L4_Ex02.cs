f6cca6f7e2ad430a8a515862774623d0f11d5e4f
Fernando Figueiredo <fernando.fig@gmail.com>
2024-09-01 23:06:49 -0300

    reorganize folder structure

---
+++ PRJ39076_L4_Ex02/C39076_L4_Ex02.cs
@@ -0,0 +1,30 @@
+﻿namespace PRJ39076_L4_Ex02
+{
+    internal class C39076_L4_Ex02
+    {
+        static void Main(string[] args)
+        {
+            char sexo = 'F';
+
+            string repetir = "S";
+
+            while (repetir == "S")
+            {
+                
+                Console.Write("Insira o sexo (F/M): ");
+                sexo = char.Parse(Console.ReadLine());                
+
+                if (sexo != 'F' && sexo != 'M')
+                {                   
+                    Console.Write("Insira um valor válido!");                   
+                    Console.ReadKey();
+                    return;
+                }
+
+                Console.Write("Deseja repetir? (S/N): ");
+                repetir = Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
